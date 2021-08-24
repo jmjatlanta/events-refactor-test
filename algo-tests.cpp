40be39f83683336f20ffcbedc2b8c95864726acd
DeckerSU <deckersu@protonmail.com>
2021-08-25 00:05:53 +0300

    algo_tests re-format

--- algo-tests.cpp
+++ algo-tests.cpp
@@ -9,7 +9,7 @@ int main() {
     std::vector<int> vw = { 12, 15, 51, 17, 17, 19, 20, 22, 23, 26, 29, 35, 40 };
     //sort(vw.begin(), vw.end(), std::greater<int>());
     sort(vw.begin(), vw.end());
-    
+
     std::ostream_iterator<int> out(std::cout, " ");
     copy(vw.begin(), vw.end(), out);
     std::cout << std::endl;
@@ -17,7 +17,7 @@ int main() {
     int value = 19;
     if (std::binary_search(vw.begin(), vw.end(), value))
         std::cout << "binary_search(" << value << ") = true" << std::endl;
-    
+
     for (value = 21; value < 23; ++value) {
         std::vector<int>::iterator it = std::lower_bound(vw.begin(), vw.end(), value);
         if (it != vw.end()) {
@@ -34,8 +34,8 @@ int main() {
         std::pair<std::vector<int>::iterator, std::vector<int>::iterator> it = std::equal_range(vw.begin(), vw.end(), value);
         if (it.first != it.second) {
             // value found
-            std::cout << "Value (" << value << ") found! it.first = " << 
-                        *(it.first) << ", it.second = " << 
+            std::cout << "Value (" << value << ") found! it.first = " <<
+                        *(it.first) << ", it.second = " <<
                         (it.second != vw.end() ? std::to_string(*(it.second)) : std::string("end")) <<
                         " [" << std::distance(it.first, it.second) << "]" <<
                         std::endl;
